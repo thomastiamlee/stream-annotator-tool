ef4b8a329cf2058358ce2f9ebd22b016f090a1d0
Thomas <thomasjamestiamlee@gmail.com>
2017-06-12 10:36:37 +0900

    Added check for unique target <div>.

--- stream-annotator-tool.js
+++ stream-annotator-tool.js
@@ -1,3 +1,8 @@
 streamAnnotatorToolInitialize = function() {
-	
+	target = $("div#stream-annotator-tool");
+	/* Check if there is only one target div in the page */
+	if (target.size() != 1) {
+		console.error("Error initializing stream-annotator-tool. No unique <div> with id stream-annotator-tool found.");
+		return;
+	}
 };
