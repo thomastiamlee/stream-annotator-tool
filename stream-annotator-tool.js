311daac192166be87ec84313a4480da32ff20a7c
Thomas <thomasjamestiamlee@gmail.com>
2017-06-12 10:42:38 +0900

    Added slider control for annotation.

--- stream-annotator-tool.js
+++ stream-annotator-tool.js
@@ -1,8 +1,17 @@
 streamAnnotatorToolInitialize = function() {
+	// Initialize the UI of the annotator tool.
+	function initializeUI() {
+		// Create the slider.
+		target.append($("<input />")
+			.attr({id: "stream-annotator-tool-slider", type: "range", min: "0", max: "100", step: "1"})
+			)
+	}
+	
 	target = $("div#stream-annotator-tool");
-	/* Check if there is only one target div in the page */
-	if (target.size() != 1) {
+	// Check if there is only one target div in the page.
+	if (target.length != 1) {
 		console.error("Error initializing stream-annotator-tool. No unique <div> with id stream-annotator-tool found.");
 		return;
 	}
+	initializeUI();
 };
